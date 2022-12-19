bf1924845cc18f58ae66543946e12619044bb182
Raja-022 <120736055+Raja-022@users.noreply.github.com>
2022-12-19 17:43:32 +0530

    Create RemoveDuplicateElement.c

---
+++ day4/RemoveDuplicateElement.c
@@ -0,0 +1,9 @@
+int removeDuplicates(int* nums, int numsSize){
+    int i=1;
+    for(int j=1;j<=numsSize;i++){
+        if(nums[j]!=nums[j-1]){
+            nums[i++]=nums[j];
+        }
+    }
+    return i;
+}
